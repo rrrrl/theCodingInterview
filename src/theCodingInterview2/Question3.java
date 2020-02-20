90c544b31d335a0159c27c5a4eba3e3e38d657c3
Jeong Haeyeon <jeonghy0709@naver.com>
2020-02-20 21:20:38 +0900

    Question 3. 중간 노드 삭제

---
+++ src/theCodingInterview2/Question3.java
@@ -0,0 +1,23 @@
+package theCodingInterview2;
+
+import CtCILibrary.*;
+import CtCILibrary.LinkedListNode;;
+
+public class Question3 {
+	public static boolean deleteNode(LinkedListNode n) {
+		if(n == null | n.next == null) {
+			return false;
+		}
+		LinkedListNode next = n.next;
+		n.data = next.data;
+		n.next = next.next;
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		LinkedListNode head = AssortedMethods.randomLinkedList(10, 0, 10);
+		System.out.println(head.printForward());
+		deleteNode(head.next.next.next); // delete node 4
+		System.out.println(head.printForward());
+	}
+}
